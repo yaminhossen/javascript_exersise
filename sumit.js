e7ffd0395f250878675306e3402040fa0c7e36d9
yaminhossen <streatdot616@gmail.com>
2024-01-04 09:34:04 +0600

    last updatec

--- sumit.js
+++ sumit.js
@@ -1,5 +1,23 @@
- const nums = [3, 5,2,1,33,44,22];
+const nums = [3, 5, 2, 1, 33, 44, 22];
 
- console.log(nums.sort(function(a,b){
-    return a-b;
- }));
+//  console.log(nums.sort(function(a,b){
+//     return a-b;
+//  }));
+
+// let name = 'I love bangladesh';
+
+//  console.log(Array.from(name));
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+function countV(name) {
+    let c = 0;
+    const letters = Array.from(name);
+    // console.log(letters);
+   letters.forEach((value) =>{
+    if(vowels.includes(value)){
+       c++;
+    }
+   })
+   return c;
+
+}
+console.log(countV('I love bangladesh'));
